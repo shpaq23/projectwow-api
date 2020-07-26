2fd635edb64bc63af41dcdb5bf8f2d8977cf1321
Michal Szpak <michal.szpak@softwareplant.com>
2020-07-26 12:26:58 +0200

    logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CharacterModule } from './character/character.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [
     AuthModule,
     CharacterModule
+  ],
+  providers: [
+    { provide: APP_FILTER, useClass: HttpErrorFilter },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  require('dotenv').config();
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  await app.listen(3000);
+    require('dotenv').config();
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.setGlobalPrefix('api');
+    await app.listen(3000);
 }
+
 bootstrap();
